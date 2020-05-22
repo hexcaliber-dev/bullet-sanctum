1c5d8926f32532c6b8714eea79e56c317d095782
Epicrider <omarepic1@gmail.com>
2020-05-22 16:48:58 -0700

    Add Smooth Transition between Two Rooms

--- Assets/Scripts/RoomSwitch.cs
+++ Assets/Scripts/RoomSwitch.cs
@@ -2,34 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RoomSwitch : MonoBehaviour
 {
 
+    public byte transitionSpeed;
+    public Image fadeImage;
+
+    bool switchNow;
     public string sceneName;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        switchNow = false;
+        fadeImage.color = new Color32(0, 0, 0, 255);
+        StartCoroutine(fadeIn());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (switchNow)
+        {
+            loadScene();
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log("RoomSwitch Collision Detected...");
-        loadScene();
+        StartCoroutine(fadeOut());
     }
 
     void loadScene()
     {
         Debug.Log("Scene Changing to... " + sceneName);
         SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator fadeOut()
+    {
+        Debug.Log("Fading Out... Start");
+
+        for (byte i = 0; i < 255; i += transitionSpeed)
+        {
+            fadeImage.color = new Color32(0, 0, 0, i);
+            yield return null;
+        }
+        switchNow = true;
+        Debug.Log("Fading Out... End");
+    }
+
+    IEnumerator fadeIn()
+    {
+        Debug.Log("Fading In... Start");
+
+        for (byte i = 255; i > 0; i -= transitionSpeed)
+        {
+            fadeImage.color = new Color32(0, 0, 0, i);
+            yield return null;
+        }
+
+        Debug.Log("Fading In... End");
     }
 
 
