b342991531c79b4a5662a0378552f903b8a554ae
Ben Cuan <github@bencuan.me>
2020-06-06 19:37:42 -0700

    Fix fracture shockwave

--- Assets/Scripts/Weapons/FractureShockBullet.cs
+++ Assets/Scripts/Weapons/FractureShockBullet.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class FractureShockBullet : Bullet {
     public float growRate;
 
-
     void Update () { 
         transform.localScale += new Vector3(1, 1, 1) * growRate * Time.deltaTime;
     }
