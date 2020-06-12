2cfcc316ed1a112d95c290c66116ace9b4d9d0dc
Joelrodiel <joel.rodiel.lucero@gmail.com>
2020-06-11 23:14:07 -0700

    Implemented Ultimate Boss Wraith spawning.

--- Assets/Scripts/Entities/UltimateBoss.cs
+++ Assets/Scripts/Entities/UltimateBoss.cs
@@ -1,23 +1,54 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UltimateBoss : Enemy {
 
+    private bool cameraStop = false;
     private bool attack_state = false;
+    private int attack_pattern = 1;
     private int attack_timr = 100;
     Animator animator;
+    private List<Transform> wraith_spawns;
+
+    // Enemies to spawn on attack #1 (Usually Wraiths).
+    public GameObject spawnee;
 
     public override void OnSpawn() {
         rb = gameObject.GetComponent<Rigidbody2D> ();
         animator = GetComponent<Animator> ();
+    
+        wraith_spawns = new List<Transform>();
+
+        foreach (Transform child in transform) {
+            wraith_spawns.Add(child);
+        }
     }
 
     public override void Attack() {
         animator.SetBool("attacking", true);
+
+        switch (attack_pattern) {
+            case 1:
+                attack1();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void attack1() {
+        // Spawn Wraiths...
+        attack_timr = 70;
+
+        GameObject spawnee1 = Instantiate(spawnee, transform.position, Quaternion.identity) as GameObject;
+        spawnee1.SendMessage("setTeleports", wraith_spawns);
     }
 
     public override void MovePattern() {
-        if (!attack_state) {
+        cameraStop = Camera.main.GetComponent<CameraUtils>().hitBoundsRight;
+
+        if (!attack_state && !cameraStop) {
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
 
@@ -26,8 +57,6 @@ public class UltimateBoss : Enemy {
         if (attack_timr < 0) {
             if (!attack_state) {
                 Attack();
-
-                attack_timr = 70;
 
                 attack_state = true;
             } else {
