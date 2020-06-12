634622562e628eb4c10250a390d2d00ff05f07c0
Joelrodiel <joel.rodiel.lucero@gmail.com>
2020-06-11 19:18:33 -0700

    Implemented Ultimate Boss behavior.

--- Assets/Scripts/Entities/UltimateBoss.cs
+++ Assets/Scripts/Entities/UltimateBoss.cs
@@ -3,15 +3,40 @@ using UnityEngine;
 
 public class UltimateBoss : Enemy {
 
+    private bool attack_state = false;
+    private int attack_timr = 100;
+    Animator animator;
+
     public override void OnSpawn() {
         rb = gameObject.GetComponent<Rigidbody2D> ();
+        animator = GetComponent<Animator> ();
     }
 
     public override void Attack() {
-
+        animator.SetBool("attacking", true);
     }
 
     public override void MovePattern() {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        if (!attack_state) {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        attack_timr -= 1;
+
+        if (attack_timr < 0) {
+            if (!attack_state) {
+                Attack();
+
+                attack_timr = 70;
+
+                attack_state = true;
+            } else {
+                animator.SetBool("attacking", false);
+
+                attack_state = false;
+
+                attack_timr = 200;
+            }
+        }
     }
 }
