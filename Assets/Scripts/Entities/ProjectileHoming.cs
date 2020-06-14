5c5e66a8711e7b3796147e89a2a51c97345d3540
Joelrodiel <joel.rodiel.lucero@gmail.com>
2020-06-13 17:28:59 -0700

    Created Homing Projectile entity.

---
+++ Assets/Scripts/Entities/ProjectileHoming.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class ProjectileHoming : Enemy {
+
+	public Transform target;
+	public float rotSpeed = 2000f;
+
+	public override void OnSpawn() {
+        rb = gameObject.GetComponent<Rigidbody2D> ();
+		rb.gravityScale = 0;
+	}
+
+	public override void Attack() {}
+
+	public override void MovePattern() {
+		rb.velocity = transform.up * speed * Time.deltaTime;
+		Vector3 targetVector = target.position - transform.position;
+		float rotatingIndex = Vector3.Cross(targetVector, transform.up).z;
+		rb.angularVelocity = -1 * rotatingIndex * rotSpeed * Time.deltaTime;
+		speed += 0.1f;
+	}
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player") {
+			player.TakeDamage(DMG);
+			Destroy(gameObject);
+		}
+		if (collision.gameObject.tag.Equals ("Bullet")) {
+			Bullet b = collision.gameObject.GetComponent<Bullet> ();
+            TakeDamage(b);
+			b.DoCollision(gameObject);
+        }
+	}
+}
