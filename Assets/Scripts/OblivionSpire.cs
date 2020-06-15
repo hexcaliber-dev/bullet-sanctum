14465216a58c1d5d02404a935d680d870e58579a
Epicrider <omarepic1@gmail.com>
2020-06-14 22:29:43 -0700

    Add Bobbing and Trascribing to Oblivion Ribosome ;)

---
+++ Assets/Scripts/OblivionSpire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OblivionSpire : MonoBehaviour
+{
+    public float transcriptionSpeed;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Vector3.up * Time.deltaTime * transcriptionSpeed);
+    }
+}
